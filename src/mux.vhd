b8212ab0bc676054726507468b2bbec39c653b18
Jens Nazarenus <me@jens-na.de>
2016-12-09 11:38:12 +0100

    Add 16:1 mux

--- src/mux.vhd
+++ src/mux.vhd
@@ -1,13 +1,17 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+use ieee.numeric_std.all;
+use ieee.std_logic_unsigned.all;
 
 entity mux is
---  Port ( );
+  Port ( 
+    selector : in std_logic_vector(3 downto 0);
+    x : in std_logic_vector(15 downto 0);
+    y : out std_logic
+  );
 end mux;
 
 architecture Behavioral of mux is
-
 begin
-
-
+    y <= x(to_integer(unsigned(selector)));
 end Behavioral;
